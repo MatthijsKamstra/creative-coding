b56e73739c1ae778f7a0292756de49b9ae32d4c4
Matthijs Kamstra <matthijs@fonk.amsterdam>
2019-01-25 11:26:10 +0100

    fixed bounce bug, documentation

--- src/util/AnimateUtil.hx
+++ src/util/AnimateUtil.hx
@@ -1,10 +1,8 @@
 package util;
 
 class AnimateUtil {
-	public function new() {
 
-	}
-
+	public function new() {	}
 
 	public static function tween(pos:Float, target, ?speed) {
 		if (speed == null) speed = 20;
@@ -12,16 +10,23 @@ class AnimateUtil {
 		return pos;
 	}
 
-	public static function bounce(num, min, max, sz) {
-		if (sz == null) {
-			sz = 0;
-		}
+	/**
+	 * check if this `num` is still inbetween `min` and `max` return a true, otherwise a false
+	 *
+	 * primarily used for bouncing balls
+	 *
+	 * @param num	value to check
+	 * @param min	minimum value
+	 * @param max	maximum value
+	 * @param sz	(optional) works with centered shapes, is the width of this shape
+	 * @return Bool
+	 */
+	public static function bounce(num:Float, min:Float, max:Float, ?sz:Float = 0) : Bool {
 		if (num >= max - sz / 2 || num - sz / 2 <= min) {
-			return 1;
+			return true;
 		} else {
-			return 0;
+			return false;
 		}
-		// return num > max ? -1 : num < min ? -1 : 1
 	}
 
 }
