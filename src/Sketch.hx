f2d20e6e1733bd80a79dcb79e7eab5e11eed4d66
Matthijs Kamstra <matthijs@fonk.amsterdam>
2019-01-26 22:10:47 +0100

    formatting, added fullscreen mode

--- src/Sketch.hx
+++ src/Sketch.hx
@@ -6,41 +6,32 @@ import js.html.*;
 import js.html.MouseEvent;
 import js.html.Event;
 import js.html.CanvasElement;
-
 import Global.*;
 
+/**
+ * inspired by George Gally, which was inpired by Seb Lee
+ * https://github.com/GeorgeGally/creative_coding/blob/master/js/canvas.js
+ *
+ * The name is again inspired by ...
+ */
 class Sketch {
-
 	public var window = js.Browser.window;
 	public var document = js.Browser.document;
+	public var canvas:js.html.CanvasElement;
 
-	public var MOUSE_DOWN : String = 'mousedown';
-	public var MOUSE_UP : String = 'mouseup';
-	public var MOUSE_MOVE : String = 'mousemove';
-	public var KEY_DOWN : String = 'keydown';
-	public var KEY_UP : String = 'keyup';
+	public function new() {}
 
-	// https://github.com/GeorgeGally/creative_coding/blob/master/js/canvas.js
-
-	var canvas : js.html.CanvasElement;
-
-	public function new() {	}
-
-	public static function create (name:String, ?type:String = '2d'):CanvasRenderingContext2D{
-
+	public static function create(name:String, ?type:String = '2d'):CanvasRenderingContext2D {
 		new Sketch().init();
-
-		if(type == '2d'){
+		if (type == '2d') {
 			return new Sketch().createCanvas(name);
 		} else {
 			return new Sketch().createGLCanvas(name);
-			// trace('return new Sketch().createGLCanvas(name);');
-			// return null;
 		}
 	}
 
 	// this enables me to have many canvases all positioned on top of eachother at 100% width and height of page
-	public function createCanvas(canvas_name):CanvasRenderingContext2D{
+	public function createCanvas(canvas_name):CanvasRenderingContext2D {
 		canvas = document.createCanvasElement();
 		var body = document.querySelector('body');
 		canvas.setAttribute("id", canvas_name);
@@ -48,14 +39,13 @@ class Sketch {
 		canvas.style.left = "0px";
 		canvas.style.top = "0px";
 		body.appendChild(canvas);
-		var ctx : CanvasRenderingContext2D = canvas.getContext('2d');
+		var ctx:CanvasRenderingContext2D = canvas.getContext('2d');
 		resize();
-		window.addEventListener("resize", resize, false);
+		window.addEventListener(RESIZE, resize, false);
 		return ctx;
 	}
 
-
-	public function createGLCanvas(canvas_name):CanvasRenderingContext2D{
+	public function createGLCanvas(canvas_name):CanvasRenderingContext2D {
 		canvas = document.createCanvasElement();
 		var body = document.querySelector('body');
 		canvas.setAttribute("id", canvas_name);
@@ -63,29 +53,29 @@ class Sketch {
 		canvas.style.left = "0px";
 		canvas.style.top = "0px";
 		body.appendChild(canvas);
-		var gl : CanvasRenderingContext2D = canvas.getContext('webgl');
-		if (gl == null) var gl = canvas.getContext('experimental-webgl');
+		var gl:CanvasRenderingContext2D = canvas.getContext('webgl');
+		if (gl == null)
+			var gl = canvas.getContext('experimental-webgl');
 		resize();
-		window.addEventListener("resize", resize, false);
+		window.addEventListener(RESIZE, resize, false);
 		return gl;
 	}
 
-	function resize(){
-		var c : Array<CanvasElement> = cast document.getElementsByTagName('canvas');
+	function resize() {
+		var c:Array<CanvasElement> = cast document.getElementsByTagName('canvas');
 		width = w = window.innerWidth;
 		height = h = window.innerHeight;
-		for (i in 0...c.length){
+		for (i in 0...c.length) {
 			var _c = c[i];
 			_c.width = width;
 			_c.height = height;
 		}
-		console.log("resize: " + w +":" + h);
+		console.log("resize: " + w + ":" + h);
 	}
 
-
-	function createHiddenCanvas(canvas_name){
+	function createHiddenCanvas(canvas_name) {
 		var ctx = createCanvas(canvas_name);
-		canvas.style.left = -w+"px";
+		canvas.style.left = -w + "px";
 		return ctx;
 	}
 
@@ -99,43 +89,59 @@ class Sketch {
 			// trace(mouseX, mouseY);
 		});
 
-		window.addEventListener(MOUSE_DOWN, function(e){
-
+		window.addEventListener(MOUSE_DOWN, function(e) {
 			mouseDown = true;
-
-			trace('${mouseDown}');
+			// trace('${mouseDown}');
 			// if(typeof onMouseDown == 'function') onMouseDown();
 		});
 
-		window.addEventListener(MOUSE_UP, function(e){
+		window.addEventListener(MOUSE_UP, function(e) {
 			mouseDown = false;
-			trace('${mouseDown}');
+			// trace('${mouseDown}');
 			// if(typeof onMouseUp == 'function') onMouseUp();
 		});
 
-		window.addEventListener(KEY_DOWN, function(e){
-			keyDown = e;
-			// if(typeof onKeyDown == 'function') onKeyDown(e);
-		});
+		window.addEventListener(KEY_DOWN, function (e:js.html.KeyboardEvent){
+			if (e.metaKey == true && e.key == 'f'){
+				if(!isFullscreen){
+					openFullscreen();
+					isFullscreen = true;
+				} else {
+					closeFullscreen();
+					isFullscreen = false;
+				}
+			}
+		}, false);
 
-		window.addEventListener(KEY_UP, function(e){
-			// if(typeof onKeyUp == 'function') onKeyUp(e);
-		});
-
-
-
+		// window.addEventListener(KEY_UP, function(e){
+		// 	// if(typeof onKeyUp == 'function') onKeyUp(e);
+		// });
 	}
 
+	/* View in fullscreen */
+	function openFullscreen() {
+		var elem = document.documentElement;
+		if (elem.requestFullscreen != null) {
+			elem.requestFullscreen();
+		} else if (untyped elem.mozRequestFullScreen) { /* Firefox */
+			untyped elem.mozRequestFullScreen();
+		} else if (untyped elem.webkitRequestFullscreen) { /* Chrome, Safari and Opera */
+			untyped elem.webkitRequestFullscreen();
+		} else if (untyped elem.msRequestFullscreen) { /* IE/Edge */
+			untyped elem.msRequestFullscreen();
+		}
+	}
 
-
-	// document.onmousedown = function() {
-	// 	mousePressed = 1;
-	// 	//window.mousePressed();
-	// }
-	// document.onmouseup = function() {
-	// 	mousePressed = 0;
-	// 	mouseReleased = 1;
-	// 	//window.mouseup();
-	// }
-
+	/* Close fullscreen */
+	function closeFullscreen() {
+		if (document.exitFullscreen != null) {
+			document.exitFullscreen();
+		} else if (untyped document.mozCancelFullScreen) { /* Firefox */
+			untyped document.mozCancelFullScreen();
+		} else if (untyped document.webkitExitFullscreen) { /* Chrome, Safari and Opera */
+			untyped document.webkitExitFullscreen();
+		} else if (untyped document.msExitFullscreen) { /* IE/Edge */
+			untyped document.msExitFullscreen();
+		}
+	}
 }
