d39bbd013d6417f1d5da7de45356e4fd0c2436e8
Matthijs Kamstra <matthijs@fonk.amsterdam>
2019-01-27 00:35:50 +0100

    added point

--- src/AST.hx
+++ src/AST.hx
@@ -1,17 +1,21 @@
 package;
 
-class AST {
+class AST {}
 
-}
-
+// point
+typedef Point = {
+	@:optional var _id:Int;
+	var x:Float;
+	var y:Float;
+};
 
 // create a Ball typedef
 typedef Ball = {
 	@:optional var id:Int;
-	var x: Float;
-	var y: Float;
-	var speed_x: Float;
-	var speed_y: Float;
-	var size: Float;
-	var colour: String;
+	var x:Float;
+	var y:Float;
+	var speed_x:Float;
+	var speed_y:Float;
+	var size:Float;
+	var colour:String;
 };
