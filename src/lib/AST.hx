53d0ffabd1a3f7f5cdbc2ab29630a0830db8d4c1
Matthijs Kamstra <matthijs@fonk.amsterdam>
2019-02-02 17:12:29 +0100

    trying to create reasonable AST for this project

--- src/lib/AST.hx
+++ src/lib/AST.hx
@@ -2,22 +2,95 @@ package lib;
 
 class AST {}
 
-// point
-typedef Point = {
+// ____________________________________ extending typedef ____________________________________
+@:enum abstract ShapeType(String) {
+	var CIRCLE = 'circle';
+	var RECTANGLE = 'rectangle';
+	var SQUARE = 'square';
+}
+
+typedef Base = {
 	@:optional var _id:Int;
+	@:optional var _type:String; // make possible to switch draw
+	// [mck] perhaps enum? ShapeType
+}
+
+typedef Dimensions = {
+	@:optional var width:Float;
+	@:optional var height:Float;
+	@:optional var rotation:Float; // not sure about this
+};
+
+typedef Position = {
 	var x:Float;
 	var y:Float;
 };
 
-// create a Ball typedef
-typedef Ball = {
-	@:optional var _id:Int;
+typedef Appearance = {
+	@:optional var color:Int;
+	@:optional var colour:String;
+	@:optional var alpha:Float;
+};
+
+// point
+typedef Point = {
 	var x:Float;
 	var y:Float;
+};
+
+// ____________________________________ shape typedef  ____________________________________
+
+typedef Circle  = {
+	> Base,
+	> Position,
+	> Appearance,
+	@:optional var radius:Float;
+	@:optional var size:Float;
+};
+
+typedef Oval  = {
+	> Base,
+	> Position,
+	> Appearance,
+	> Dimensions,
+};
+
+typedef Square = {
+	> Base,
+	> Position,
+	> Appearance,
+	@:optional var rotation:Float;
+	@:optional var size:Float; // width and height are the same
+}
+typedef Rectangle = {
+	> Base,
+	> Position,
+	> Appearance,
+	> Dimensions,
+};
+
+typedef LineNew = {
+	> Base,
+	> Appearance,
+	var x1:Float;
+	var y1:Float;
+	var x2:Float;
+	var y2:Float;
+	@:optional var point1:Point;
+	@:optional var point2:Point;
+	@:optional var pointArray:Array<Point>;
+	@:optional var lineWeight:Int;
+	@:optional var strokeColor:Int;
+	@:optional var strokeWeight:Int;
+	@:optional var lineEnd:String;
+};
+
+// create a Ball typedef
+typedef Ball = {
+	> Circle,
 	var speed_x:Float;
 	var speed_y:Float;
-	var size:Float;
-	var colour:String;
+	// var colour:String;
 };
 
 // create a Line typedef
