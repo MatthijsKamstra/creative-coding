07ee39ffd3951942f1efc4336cdb1eedac54fbed
Matthijs Kamstra <matthijs@fonk.amsterdam>
2019-02-08 10:12:03 +0100

    update to base template

--- src/art/CC030_.hx
+++ src/art/CC030_.hx
@@ -6,7 +6,13 @@ package art;
 class CC020 extends CCBase implements ICCBase {
 
 	var shapeArray : Array<Circle> = [];
+	var grid : GridUtil = new GridUtil();
+
 	var _radius = 150;
+	var _cellsize = 150;
+	var _bgColor:RGB = null;
+	var _lineColor:RGB = null;
+	var _fillColor:RGB = null;
 
 	public function new(ctx:CanvasRenderingContext2D) {
 		super(ctx);
@@ -32,16 +38,35 @@ class CC020 extends CCBase implements ICCBase {
 	function drawShape(){
 		ctx.clearRect(0,0,w,h);
 		ctx.backgroundObj(WHITE);
-		for ( i in 0 ... shapeArray.length ) {
+
+
+		if (isDebug) {
+			ShapeUtil.gridField(ctx, grid);
 		}
+
+		for ( i in 0 ... shapeArray.length ) {
+			var circle = shapeArray[i];
+		}
+		var rgb = randomColourObject();
+		ctx.strokeColour(rgb.r, rgb.g, rgb.b);
+		ctx.xcross(w/2, h/2, 200);
 	}
 
 
 	override function setup(){
 		trace('setup: ${toString()}');
-		var rgb = randomColourObject();
-		ctx.strokeColour(rgb.r, rgb.g, rgb.b);
-		ctx.xcross(w/2, h/2, 200);
+
+		var colorArray = lib.util.ColorUtil.niceColor100[randomInt(lib.util.ColorUtil.niceColor100.length-1)];
+		_bgColor = hex2RGB(colorArray[0]);
+		_lineColor = hex2RGB(colorArray[1]);
+		_fillColor = hex2RGB(colorArray[2]);
+
+		// isDebug = true;
+
+		// grid.setDimension(w*2.1, h*2.1);
+		// grid.setNumbered(3,3);
+		grid.setCellSize(_cellsize);
+		grid.setIsCenterPoint(true);
 	}
 
 	override function draw(){
