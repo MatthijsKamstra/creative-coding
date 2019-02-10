2754c7ada65ea39cddb45e59c901cc31ea898823
Matthijs Kamstra <matthijs@fonk.amsterdam>
2019-02-10 13:22:15 +0100

    formatting

--- src/art/CC026.hx
+++ src/art/CC026.hx
@@ -4,19 +4,15 @@ package art;
  * short description what this does
  */
 class CC026 extends CCBase implements ICCBase {
-
-	var shapeArray : Array<Circle> = [];
-	var grid : GridUtil = new GridUtil();
-
+	var shapeArray:Array<Circle> = [];
+	var grid:GridUtil = new GridUtil();
 	var shapeMax = 2000;
-
 	var _radius = 150;
 	var _cellsize = 150;
 	var _bgColor:RGB = null;
 	var _lineColor:RGB = null;
 	var _fillColor:RGB = null;
-
-	var ctx2: CanvasRenderingContext2D;
+	var ctx2:CanvasRenderingContext2D;
 
 	public function new(ctx:CanvasRenderingContext2D) {
 		super(ctx);
@@ -34,42 +30,41 @@ class CC026 extends CCBase implements ICCBase {
 	// 	// onAnimateHandler(shape);
 	// 	return shape;
 	// }
-
-	function onAnimateHandler(circle:Circle){
+	function onAnimateHandler(circle:Circle) {
 		var startx = circle.x;
 		var starty = circle.y;
 
-		if(posNeg() > 0 ){
-			startx = (circle.x < w/2) ? circle.x - w: circle.x + w;
-			starty += random(-h,h);
-		} else{
-			startx += random(-w,w);
-			starty = (circle.y < h/2) ? circle.y - h: circle.y + h;
+		if (posNeg() > 0) {
+			startx = (circle.x < w / 2) ? circle.x - w : circle.x + w;
+			starty += random(-h, h);
+		} else {
+			startx += random(-w, w);
+			starty = (circle.y < h / 2) ? circle.y - h : circle.y + h;
 		}
 
-		var time = random(1,3);
-		var delay = random(0,3);
+		var time = random(1, 3);
+		var delay = random(0, 3);
 
-		GoJs.from(circle, time).delay(delay).x(startx).y(starty).ease(lets.easing.Sine.easeOut);
+		GoJs.from(circle, time)
+			.delay(delay)
+			.x(startx)
+			.y(starty)
+			.ease(lets.easing.Sine.easeOut);
 	}
 
-	function drawShape(){
-		ctx.clearRect(0,0,w,h);
+	function drawShape() {
+		ctx.clearRect(0, 0, w, h);
 		ctx.backgroundObj(WHITE);
 
-
-		for ( i in 0 ... shapeArray.length ) {
+		for (i in 0...shapeArray.length) {
 			var circle = shapeArray[i];
 
 			ctx.fillStyle = circle.colour;
 			ctx.circle(circle.x, circle.y, 5);
-
 		}
-
 	}
 
-
-	override function setup(){
+	override function setup() {
 		trace('setup: ${toString()}');
 		var img = new Image();
 		// img.src = '/assets/img/rhino.jpg';
@@ -88,47 +83,45 @@ class CC026 extends CCBase implements ICCBase {
 
 			getPixel();
 		};
-
 	}
 
-	function getPixel(){
-		for (i in 0...shapeMax){
-			var xpos = random(0,ctx2.canvas.width);
-			var ypos = random(0,ctx2.canvas.height);
+	function getPixel() {
+		for (i in 0...shapeMax) {
+			var xpos = random(0, ctx2.canvas.width);
+			var ypos = random(0, ctx2.canvas.height);
 
 			var pixel = ctx2.getImageData(xpos, ypos, 1, 1);
 			var data = pixel.data;
-			if(data[0] == 255 && data[1] == 255 && data[2] == 255) continue;
-			var rgba = 'rgba(' + data[0] + ', ' + data[1] +
-						', ' + data[2] + ', ' + (data[3] / 255) + ')';
+			if (data[0] == 255 && data[1] == 255 && data[2] == 255)
+				continue;
+			var rgba = 'rgba(' + data[0] + ', ' + data[1] + ', ' + data[2] + ', ' + (data[3] / 255) + ')';
 			// trace(rgba);
 
-			var xstart = Math.round((w - ctx2.canvas.width)/2);
-			var ystart = Math.round((h- ctx2.canvas.height)/2);
+			var xstart = Math.round((w - ctx2.canvas.width) / 2);
+			var ystart = Math.round((h - ctx2.canvas.height) / 2);
 
-			var circle : Circle = {
+			var circle:Circle = {
 				_id: '$i',
 				_type: 'circle',
 				x: xstart + xpos,
 				y: ystart + ypos,
-				radius:10,
-				colour:rgba,
+				radius: 10,
+				colour: rgba,
 			}
 
 			shapeArray.push(circle);
 			// ctx.strokeCircle(circle.x, circle.y, 10);
-
 		}
 
 		// console.table(shapeArray);
 		trace(shapeArray.length);
-		for (i in 0...shapeArray.length){
+		for (i in 0...shapeArray.length) {
 			var _shapeArray = shapeArray[i];
 			onAnimateHandler(_shapeArray);
 		}
 	}
 
-	override function draw(){
+	override function draw() {
 		// trace('draw: ${toString()}');
 		drawShape();
 		// stop();
