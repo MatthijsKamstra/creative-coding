a09e1321f27db24d8fcc31a1cc3517eb3b2fffa1
Matthijs Kamstra <matthijs@fonk.amsterdam>
2019-02-13 20:34:47 +0100

    make sure the start of the dashed line is hidden, refresh build with space, formatting

--- src/art/CC030.hx
+++ src/art/CC030.hx
@@ -18,6 +18,8 @@ class CC030 extends CCBase implements ICCBase {
 	public function new(ctx:CanvasRenderingContext2D) {
 		super(ctx);
 		description = '';
+
+		window.addEventListener(KEY_DOWN, onKeyDown);
 	}
 
 	function createShape(i:Int, ?point:Point) {
@@ -38,14 +40,13 @@ class CC030 extends CCBase implements ICCBase {
 	// function onAnimateHandler(arc:Arc){
 	// 	GoJs.to(arc, random).x(w/2).y(h/2).ease(lets.easing.Sine.easeInOut).onComplete(onAnimateHandler, [arc]);
 	// }
-
 	function drawShape() {
 		ctx.clearRect(0, 0, w, h);
 		ctx.backgroundObj(BLACK);
 
 		for (i in 0...shapeArray.length) {
 			var sh = shapeArray[i];
-			sh.rotation += radians(randomInt(360));
+			sh.rotation = radians(randomInt(360));
 			// sh.rotation += sh.speed + (i * 1);
 
 			ctx.strokeStyle = getColourObj(WHITE);
@@ -58,7 +59,7 @@ class CC030 extends CCBase implements ICCBase {
 
 			ctx.save();
 			ctx.translate(sh.x, sh.y);
-			ctx.rotate(radians(sh.rotation) );
+			ctx.rotate(sh.rotation);
 			// ctx.centreFillRect(0, 0, sh.width, sh.height);
 			// ctx.strokeCircle(sh.x, sh.y, _radius);
 			ctx.strokeCircle(0, 0, _radius);
@@ -68,30 +69,30 @@ class CC030 extends CCBase implements ICCBase {
 		}
 	}
 
-	function createDashedArray(radius:Float):Array<Float>{
+	function createDashedArray(radius:Float):Array<Float> {
 		var omtrek = (MathUtil.circumferenceCircle(radius));
 		// trace('omtrek: $omtrek');
-		var arr : Array<Float> = [];
+		var arr:Array<Float> = [];
 		var randomParts = randomInt(0, 50);
-		if(randomParts % 2 != 0) randomParts++;
+		if (randomParts % 2 != 0)
+			randomParts++;
 		// trace('randomParts: $randomParts');
 		var counter = 0;
-		for ( i in 0 ... randomParts ) {
-			var value = randomInt(0,100);
+		for (i in 0...randomParts) {
+			var value = randomInt(0, 100);
 			counter += value;
 			arr.push(value);
 		}
 		// trace('counter: $counter');
-		var temp =  omtrek/ counter;
+		var temp = omtrek / counter;
 		// trace('temp ($omtrek/$counter): $temp');
 
-		for ( i in 0 ... arr.length ) {
+		for (i in 0...arr.length) {
 			arr[i] = arr[i] * temp;
 		}
 		// trace('arr: $arr');
 		return arr;
 	}
-
 
 	override function setup() {
 		trace('setup: ${toString()}');
@@ -104,18 +105,26 @@ class CC030 extends CCBase implements ICCBase {
 			shapeArray.push(createShape(i));
 		}
 
-
 		// trace(1 % 2); // 1
 		// trace(2 % 2); // 0
 		// trace(3 % 2); // 1
 		// trace(4 % 2); // 0
 		// trace(40152 % 2); // 0
-
 	}
 
 	override function draw() {
 		// trace('draw: ${toString()}');
 		drawShape();
 		stop();
+	}
+
+	// ____________________________________ onhandler ____________________________________
+	function onKeyDown(e:js.html.KeyboardEvent) {
+		switch (e.key) {
+			case ' ':
+				drawShape();
+			default:
+				trace("case '" + e.key + "': trace ('" + e.key + "');");
+		}
 	}
 }
