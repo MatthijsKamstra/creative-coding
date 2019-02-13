58a8291624c0afd8b7162e1158a6740242905042
Matthijs Kamstra <matthijs@fonk.amsterdam>
2019-02-13 20:35:19 +0100

    dots on a grid react on mousemove

--- src/art/CC031.hx
+++ src/art/CC031.hx
@@ -4,74 +4,108 @@ package art;
  * short description what this does
  */
 class CC031 extends CCBase implements ICCBase {
-
-	var shapeArray : Array<Circle> = [];
-	var grid : GridUtil = new GridUtil();
-
-	var _radius = 150;
-	var _cellsize = 150;
-	var _bgColor:RGB = null;
-	var _lineColor:RGB = null;
-	var _fillColor:RGB = null;
+	var shapeArray:Array<Circle> = [];
+	var grid:GridUtil = new GridUtil();
+	var minRadius = 10;
+	var maxRadius = 35;
+	var cellsize = 50;
+	var maxDistance = h;
 
 	public function new(ctx:CanvasRenderingContext2D) {
+		description = 'Circles react to mouse';
 		super(ctx);
-		description = '';
 	}
 
-	function createShape(i:Int, ?point:Point){
-		var shape : Circle = {
+	function createShape(i:Int, ?point:Point) {
+		var shape:Circle = {
 			_id: '$i',
 			_type: 'circle',
 			x: point.x,
 			y: point.y,
-			radius:_radius,
+			radius: minRadius,
 		}
 		onAnimateHandler(shape);
 		return shape;
 	}
 
-	function onAnimateHandler(circle:Circle){
-		// GoJs.to(circle, 5).x(w/2).y(h/2).ease(lets.easing.Sine.easeInOut).onComplete(onAnimateHandler, [circle]);
-	}
-
-	function drawShape(){
-		ctx.clearRect(0,0,w,h);
+	// ____________________________________ shape creation ____________________________________
+	function drawShape() {
+		ctx.clearRect(0, 0, w, h);
 		ctx.backgroundObj(WHITE);
 
-
 		if (isDebug) {
-			ShapeUtil.gridField(ctx, grid);
+			ShapeUtil.gridDots(ctx, grid);
 		}
 
-		for ( i in 0 ... shapeArray.length ) {
-			var circle = shapeArray[i];
+		for (i in 0...shapeArray.length) {
+			var sh = shapeArray[i];
+			ctx.fillColourRGB(BLACK);
+			ctx.fillCircle(sh.x, sh.y, sh.radius);
 		}
-		var rgb = randomColourObject();
-		ctx.strokeColour(rgb.r, rgb.g, rgb.b);
-		ctx.xcross(w/2, h/2, 200);
+		// var rgb = randomColourObject();
+		// ctx.strokeColour(rgb.r, rgb.g, rgb.b);
+		// ctx.xcross(w / 2, h / 2, 200);
 	}
 
-
-	override function setup(){
+	// ____________________________________ override ____________________________________
+	override function setup() {
 		trace('setup: ${toString()}');
 
-		var colorArray = lib.util.ColorUtil.niceColor100[randomInt(lib.util.ColorUtil.niceColor100.length-1)];
-		_bgColor = hex2RGB(colorArray[0]);
-		_lineColor = hex2RGB(colorArray[1]);
-		_fillColor = hex2RGB(colorArray[2]);
+		shapeArray = [];
 
 		// isDebug = true;
 
 		// grid.setDimension(w*2.1, h*2.1);
 		// grid.setNumbered(3,3);
-		grid.setCellSize(_cellsize);
+		grid.setCellSize(cellsize);
 		grid.setIsCenterPoint(true);
+
+		for (i in 0...grid.array.length) {
+			var point = grid.array[i];
+			shapeArray.push(createShape(i, point));
+		}
+
+		window.addEventListener("mousemove", onMouseMoveHandler);
 	}
 
-	override function draw(){
-		trace('draw: ${toString()}');
-		// drawShape();
-		stop();
+	override function draw() {
+		// trace('draw: ${toString()}');
+		drawShape();
+		// stop();
+	}
+
+	// ____________________________________ handler ____________________________________
+	function onMouseMoveHandler(e:js.html.MouseEvent) {
+		// trace(e.clientX, e.clientY);
+		var point:Point = {
+			x: e.clientX,
+			y: e.clientY
+		}
+		for (j in 0...shapeArray.length) {
+			var b2 = shapeArray[j];
+			if (point == b2)
+				continue;
+			var currentDistance = distance(point.x, point.y, b2.x, b2.y);
+			// trace('-----\n ${line} / ${b2} -- ${currentDistance}');
+			if (currentDistance < maxDistance) {
+
+				console.log('minRadius: $minRadius');
+				console.log('maxRadius: $maxRadius');
+				console.log('(maxRadius / minRadius): ($maxRadius / $minRadius) -> ${(maxRadius / minRadius)}');
+				console.log('(maxDistance / currentDistance): ($maxDistance / $currentDistance) -> ${(maxDistance / currentDistance)}');
+
+				// var alpha:Float = 0.8 - (currentDistance / maxDistance); // 0.5
+				// b2.radius = alpha;
+				// b2.radius = minRadius + ((maxRadius / minRadius) * (maxDistance / currentDistance));
+				// b2.radius = maxRadius - (minRadius + ((currentDistance/maxDistance) * (maxRadius-minRadius))) ;
+				b2.radius = maxRadius - ((currentDistance/maxDistance) * (maxRadius-minRadius)) ;
+			} else {
+				b2.radius = minRadius;
+			}
+		}
+	}
+
+	function onAnimateHandler(circle:Circle) {
+		// GoJs.to(circle, 5).x(w/2).y(h/2).ease(lets.easing.Sine.easeInOut).onComplete(onAnimateHandler, [circle]);
 	}
 }
