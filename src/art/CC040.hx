8d820b0150b67e91cb8483169df84358c6caac03
Matthijs Kamstra <matthijs@fonk.amsterdam>
2019-02-22 16:23:30 +0100

    fixed 040

--- src/art/CC040.hx
+++ src/art/CC040.hx
@@ -4,22 +4,14 @@ package art;
  * short description what this does
  */
 class CC040 extends CCBase implements ICCBase {
-
-	var shapeArray : Array<Circle> = [];
-	var grid : GridUtil = new GridUtil();
-
-	var max = 1;
-
-	var startRadius = 200;
-
-
-	var _radius = 150;
+	var shapeArray:Array<Circle> = [];
+	var grid:GridUtil = new GridUtil();
 	var _cellsize = 150;
-	var _color0 :RGB = null;
-	var _color1 :RGB = null;
-	var _color2 :RGB = null;
-	var _color3 :RGB = null;
-	var _color4 :RGB = null;
+	var _color0:RGB = null;
+	var _color1:RGB = null;
+	var _color2:RGB = null;
+	var _color3:RGB = null;
+	var _color4:RGB = null;
 
 	public function new(ctx:CanvasRenderingContext2D) {
 		description = 'circle lines';
@@ -27,71 +19,78 @@ class CC040 extends CCBase implements ICCBase {
 		super(ctx);
 	}
 
-	function createShape(i:Int){
-		var shape : Circle = {
+	function createShape(i:Int, point:Point) {
+		var shape:Circle = {
 			_id: '$i',
 			_type: 'circle',
-			x: w/2,
-			y: h/2,
-			radius:_radius,
+			x: point.x,
+			y: point.y,
+			radius: _cellsize / 2,
 		}
-		onAnimateHandler(shape);
+		Reflect.setField(shape, 'part', 0);
+		Reflect.setField(shape, 'dir', 1);
 		return shape;
 	}
 
-	function onAnimateHandler(circle:Circle){
-		// GoJs.to(circle, 5).x(w/2).y(h/2).ease(lets.easing.Sine.easeInOut).onComplete(onAnimateHandler, [circle]);
-	}
-
-	function drawShape(){
-		ctx.clearRect(0,0,w,h);
+	function drawShape() {
+		ctx.clearRect(0, 0, w, h);
 		ctx.backgroundObj(BLACK);
 
 		for (i in 0...shapeArray.length) {
 			var sh = shapeArray[i];
-			// sh.rotation = radians(randomInt(360));
-			// sh.rotation += sh.speed;
 
 			ctx.strokeStyle = getColourObj(WHITE);
 			ctx.lineCap = 'butt'; // "butt|round|square";
-			ctx.lineWidth =20;
+			ctx.lineWidth = 15;
 
-			var radius = startRadius;
-			var omtrek = (MathUtil.circumferenceCircle(radius));
-			ctx.setLineDash([1,Math.round(omtrek/4)]);
-			ctx.strokeCircle(sh.x, sh.y, radius);
+			// get circumference from circles
+			var omtrek:Float = MathUtil.circumferenceCircle(sh.radius);
+			var part:Float = Reflect.getProperty(sh, 'part');
+			var dir = Reflect.getProperty(sh, 'dir');
+
+			// trace(Reflect.getProperty(sh, 'part'));
+			// [mck] I have no idea why I need to sutrace `-0.5` but it fixes my little line problem
+			ctx.setLineDash([part, (omtrek / 4)-0.5 -part]);
+			ctx.strokeCircle(sh.x, sh.y, sh.radius);
+
+			if (part >= omtrek / 4) {
+				Reflect.setField(sh, 'dir', -1);
+			}
+			if (part < 0) {
+				Reflect.setField(sh, 'dir', 1);
+			}
+
+			Reflect.setField(sh, 'part', part + dir);
 		}
 	}
 
-
-	override function setup(){
+	override function setup() {
 		trace('setup: ${toString()}');
 
-		var colorArray = lib.util.ColorUtil.niceColor100[randomInt(lib.util.ColorUtil.niceColor100.length-1)];
+		var colorArray = lib.util.ColorUtil.niceColor100[randomInt(lib.util.ColorUtil.niceColor100.length - 1)];
 		_color0 = hex2RGB(colorArray[0]);
 		_color1 = hex2RGB(colorArray[1]);
 		_color2 = hex2RGB(colorArray[2]);
 		_color3 = hex2RGB(colorArray[3]);
 		_color4 = hex2RGB(colorArray[4]);
 
-
-		isDebug = true;
+		// isDebug = true;
 
 		// grid.setDimension(w*2.1, h*2.1);
 		// // grid.setNumbered(3,3);
-		// grid.setCellSize(_cellsize);
-		// grid.setIsCenterPoint(true);
+		grid.setDebug(isDebug);
+		grid.setCellSize(_cellsize);
+		grid.setIsCenterPoint(true);
 
 		shapeArray = [];
-		for (i in 0...max) {
-			shapeArray.push(createShape(i));
+		for (i in 0...grid.array.length) {
+			shapeArray.push(createShape(i, grid.array[i]));
 		}
-
 	}
 
-	override function draw(){
-		trace('draw: ${toString()}');
+	override function draw() {
+		// trace('draw: ${toString()}');
 		drawShape();
-		stop();
+		// stop();
 	}
 }
