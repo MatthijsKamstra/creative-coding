227fed3d8cd3a315e6574fade46b526bd4589a06
Matthijs Kamstra <matthijs@fonk.amsterdam>
2019-02-27 23:47:30 +0100

    update template

--- src/art/CC050_.hx
+++ src/art/CC050_.hx
@@ -7,18 +7,46 @@ class CC042 extends CCBase implements ICCBase {
 
 	var shapeArray : Array<Circle> = [];
 	var grid : GridUtil = new GridUtil();
-
+	// sizes
 	var _radius = 150;
 	var _cellsize = 150;
+	// colors
 	var _color0:RGB = null;
 	var _color1:RGB = null;
 	var _color2:RGB = null;
+	var _color3:RGB = null;
+	var _color4:RGB = null;
+
+	var panel1:QuickSettings;
+
 
 	public function new(ctx:CanvasRenderingContext2D) {
 		description = '';
 		type = [CCType.ANIMATION, CCType.IMAGE];
+
+		// <link href="https://fonts.googleapis.com/css?family=Oswald:200,300,400,500,600,700" rel="stylesheet">
+		FontUtil.embedGoogleFont('Oswald:200,300,400,500,600,700', onEmbedHandler);
+
 		super(ctx);
 	}
+
+	function onEmbedHandler(e) {
+		trace('onEmbedHandler: "${e}"');
+		drawShape();
+	}
+
+	function createQuickSettings() {
+		// demo/basic example
+		panel1 = QuickSettings.create(10, 10, "Quote generator")
+			.setGlobalChangeHandler(untyped drawShape)
+			.addHTML("Reason", "Sometimes I need a quick quote, to post on Instagram")
+
+			.addTextArea('Quote', 'text', function (value) trace(value) )
+			.addBoolean('All Caps', false, function (value) trace(value) )
+
+			.saveInLocalStorage('store-data-${toString()}');
+	}
+
 
 	function createShape(i:Int, ?point:Point){
 		var shape : Circle = {
@@ -40,6 +68,8 @@ class CC042 extends CCBase implements ICCBase {
 		ctx.clearRect(0,0,w,h);
 		ctx.backgroundObj(WHITE);
 
+		ctx.fillStyle = getColourObj(_color4);
+		FontUtil.centerFillText(ctx, 'text', w/2, h/2, "'Oswald', sans-serif;", 160);
 
 		if (isDebug) {
 			ShapeUtil.gridField(ctx, grid);
@@ -57,10 +87,12 @@ class CC042 extends CCBase implements ICCBase {
 	override function setup(){
 		trace('setup: ${toString()}');
 
-		var colorArray = lib.util.ColorUtil.niceColor100[randomInt(lib.util.ColorUtil.niceColor100.length-1)];
+		var colorArray = lib.util.ColorUtil.niceColor100SortedString[randomInt(lib.util.ColorUtil.niceColor100SortedString.length-1)];
 		_color0 = hex2RGB(colorArray[0]);
 		_color1 = hex2RGB(colorArray[1]);
 		_color2 = hex2RGB(colorArray[2]);
+		_color3 = hex2RGB(colorArray[3]);
+		_color4 = hex2RGB(colorArray[4]);
 
 		isDebug = true;
 
