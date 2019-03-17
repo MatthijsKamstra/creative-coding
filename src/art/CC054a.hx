1fd205b0a389ebe64c710fa3d96e45ba15870d29
Matthijs Kamstra <matthijs@fonk.amsterdam>
2019-03-17 21:44:42 +0100

    update with new object

--- src/art/CC054a.hx
+++ src/art/CC054a.hx
@@ -4,11 +4,15 @@ package art;
  * short description what this does
  */
 class CC054a extends CCBase implements ICCBase {
+	var shapeArray:Array<EyeObj> = [];
 	var grid:GridUtil = new GridUtil();
 	// sizes
 	var _radius = 150;
 	var _cellsize = 150;
 	var colorArr = [GREEN, PINK, BLUE, AQUA, TEAL, OLIVE, PURPLE, MAROON, GRAY];
+	var outerR = 0.0;
+	var cell = 0.0;
+	var innerR = 50.0;
 
 	public function new(ctx:CanvasRenderingContext2D) {
 		description = '';
@@ -20,50 +24,82 @@ class CC054a extends CCBase implements ICCBase {
 		// trace(shuffle);
 	}
 
+	function createShape(i:Int, ?point:Point) {
+		cell = Math.min(grid.cellHeight, grid.cellWidth);
+		outerR = Math.round(cell / 2) - 20;
+		var shape:EyeObj = {
+			_id: '$i',
+			_type: 'EyeObj',
+			// x: point.x,
+			// y: point.y,
+			// radius: _radius,
+			x: point.x,
+			y: point.y,
+			outline: {x: point.x + random(-3, 3), y: point.y + random(-3, 3)},
+			highlight: {x: point.x + (innerR - (innerR / 2)), y: point.y + (innerR - (innerR / 2))},
+			rgb: colorArr[i],
+		}
+
+		return shape;
+	}
+
 	function drawShape() {
 		ctx.clearRect(0, 0, w, h);
 		ctx.backgroundObj(WHITE);
 
-		for (i in 0...grid.array.length) {
-			createEye(i, grid.array[i]);
+		for (i in 0...shapeArray.length) {
+			createEye(i, shapeArray[i]);
 		}
 	}
 
-	function createEye(i:Int, ?point:Point) {
-		var w2 = point.x;
-		var h2 = point.y;
+	function createEye(i:Int, eye:EyeObj) {
+		// trace(Sketch.Global.mouseX);
 
-		var cell = Math.min(grid.cellHeight, grid.cellWidth);
-
-		var outerR = Math.round(cell / 2) - 20;
-		var innerR = 50;
 		// outline
 		cc.draw.Circle.create(ctx)
-			.pos(w2 + random(-3, 3), h2 + random(-3, 3))
+			.pos(eye.outline.x, eye.outline.y)
 			.fill(BLACK)
-			.radius(outerR + 10)
+			.radius(Math.round(outerR + 10))
 			.draw();
 
 		// eye color
-		cc.draw.Circle.create(ctx).pos(w2, h2).fill(colorArr[i]).radius(outerR).draw();
+		cc.draw.Circle.create(ctx)
+			.pos(eye.x, eye.y)
+			.fill(eye.rgb)
+			.radius(Math.round(outerR))
+			.draw();
 
+		// this is going to move
+		var dirX = eye.x - (innerR / 2);
+		var dirY = eye.y - (innerR / 2);
 		// pattern
 		ctx.strokeColourRGB(WHITE, 0.1);
 		for (i in 0...1000) {
-			var point = MathUtil.orbit(w2, h2, random(360), random(innerR, outerR - 5));
-			ctx.line(w2, h2, point.x, point.y);
+			var point = MathUtil.orbit(eye.x, eye.y, random(360), random(innerR, outerR - 5));
+			ctx.line(eye.x, eye.y, point.x, point.y);
 		}
 		// inner black eye..
-		cc.draw.Circle.create(ctx).pos(w2, h2).fill(BLACK).radius(innerR).draw();
-		// high light
-		var xpos = w2 - innerR + 10;
-		var ypos = h2 - innerR + 20;
+		cc.draw.Circle.create(ctx)
+			.pos(dirX, dirY)
+			.fill(BLACK)
+			.radius(Math.round(innerR))
+			.draw();
+
+		// highlight left top
+		var xpos = eye.x - innerR + 10;
+		var ypos = eye.y - innerR + 20;
 		cc.draw.Circle.create(ctx) // .pos(w2 + random(-innerR, innerR), h2 + random(-innerR, innerR))
-			.color(WHITE).pos(xpos, ypos).radius(20).alpha(0.7).draw();
+			.color(WHITE)
+			.pos(xpos, ypos)
+			.radius(20)
+			.alpha(0.7).draw();
+
 		// random blink
+
+		// highlight random
 		var out = outerR - 50;
 		cc.draw.Circle.create(ctx)
-			.pos(w2 + random(-out, out), h2 + random(-out, out))
+			.pos(eye.x + random(-out, out), eye.y + random(-out, out))
 			.color(WHITE)
 			.radius(10)
 			.alpha(0.7)
@@ -80,6 +116,11 @@ class CC054a extends CCBase implements ICCBase {
 		grid.setDimension(w - (padding * 2), h - (padding * 2));
 		grid.setNumbered(3, 3);
 		grid.setIsCenterPoint(true);
+
+		shapeArray = [];
+		for (i in 0...grid.array.length) {
+			shapeArray.push(createShape(i, grid.array[i]));
+		}
 	}
 
 	override function draw() {
@@ -88,3 +129,15 @@ class CC054a extends CCBase implements ICCBase {
 		stop();
 	}
 }
+
+/**
+ * need this if we need to animate
+ */
+typedef EyeObj = {
+	> Base,
+	> Position,
+	> Appearance,
+	var outline:Point;
+	var highlight:Point;
+	// var color:RGB;
+};
