32856058ba6c5cf3acd61fba07886653ef83daa4
Matthijs Kamstra <matthijs@fonk.amsterdam>
2019-11-12 12:00:29 +0100

    no idea why this doesn't work anymore... removed it for now

--- src/lib/model/constants/App.hx
+++ src/lib/model/constants/App.hx
@@ -4,10 +4,9 @@ import haxe.macro.Context;
 
 class App {
 	public static var NAME:String = "Creative Code [mck]";
-	public static var BUILD:String = getBuildDate();
-
-	macro public static function getBuildDate() {
-		var date = Date.now().toString();
-		return Context.makeExpr(date, Context.currentPos());
-	}
+	// public static var BUILD:String = getBuildDate();
+	// macro public static function getBuildDate() {
+	// 	var date = Date.now().toString();
+	// 	return Context.makeExpr(date, Context.currentPos());
+	// }
 }
